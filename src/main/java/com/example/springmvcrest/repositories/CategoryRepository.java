50f52a887851c415883fb02971d6dbd9b120289a
splacik992 <r.paliwoda992@gmail.com>
2021-07-14 19:14:02 +0200

    add findByName method

--- src/main/java/com/example/springmvcrest/repositories/CategoryRepository.java
+++ src/main/java/com/example/springmvcrest/repositories/CategoryRepository.java
@@ -4,4 +4,5 @@ import com.example.springmvcrest.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String name);
 }
