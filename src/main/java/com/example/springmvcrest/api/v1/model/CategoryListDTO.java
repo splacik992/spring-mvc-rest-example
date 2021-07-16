bd3dc86eb207896004dccaa7382d2f6d9020186a
splacik992 <r.paliwoda992@gmail.com>
2021-07-16 11:45:30 +0200

    Created CategoryListDto.

---
+++ src/main/java/com/example/springmvcrest/api/v1/model/CategoryListDTO.java
@@ -0,0 +1,13 @@
+package com.example.springmvcrest.api.v1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class CategoryListDTO {
+    
+    private List<CategoryDTO> categories;
+}
